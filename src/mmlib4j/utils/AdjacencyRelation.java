ddff77e1b2c80356c299ff95026af8879e636545
wonderalexandre <wonderalexandre@gmail.com>
2016-05-12 19:46:34 -0300

    minor update

--- src/mmlib4j/utils/AdjacencyRelation.java
+++ src/mmlib4j/utils/AdjacencyRelation.java
@@ -299,17 +299,7 @@ public class AdjacencyRelation {
 		}
 		return adj;
 	}
-<<<<<<< HEAD
-	
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
-	
-=======
-
->>>>>>> descriptors
-	
 	public AdjacencyRelation diagonalUpperRightSide(){
 		int cont=0;
 		for (int i=0; i < this.px.length; i++){
